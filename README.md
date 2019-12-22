6609817b0e19d59286803f3941c709d692ae2ae2
Philippe Métais <phi.metais@gmail.com>
2019-12-22 18:21:52 +0100

    :sparkles: Introducing handling of Bloom AccountRegistryLogic

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# Example Subgraph
+# Bloom Subgraph
 
-An example to help you get started with The Graph. For more information see the docs on https://thegraph.com/docs/.
+This project leverages TheGraph network to help query data from Bloom smart contracts.
+
+It was developed as a submission to [TheGraph hackathon in late 2019](https://thegraph.com/hackathons/2019/12).
+
+## Subgraph
+
+You can query this subgraph with the [online explorer](https://thegraph.com/explorer/subgraph/philippemts/bloom).
+
+## Resources
+
+- [TheGraph Protocol](https://thegraph.com/)
+- [Bloom](https://bloom.co/) 
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,22 +1,104 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+import {
+  AccountRegistryLogic,
+  AddressLinked,
+  AddressUnlinked
+} from "../generated/Account/AccountRegistryLogic";
+import {
+  BloomAccount,
+  BloomAddress,
+  BloomAddressHistoryItem
+} from "../generated/schema";
 
-export function handleNewGravatar(event: NewGravatar): void {
-  let gravatar = new Gravatar(event.params.id.toHex())
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+export function handleAddressLinked(event: AddressLinked): void {
+  // get parameters from event
+  let blockNumber = event.block.number;
+  let accountID = event.params.linkId.toHex();
+
+  // read the contract for additional state variables
+  let accountRegistryLogic = AccountRegistryLogic.bind(event.address);
+  let initializing = accountRegistryLogic.initializing();
+
+  // get or create account object
+  let account = BloomAccount.load(accountID);
+  if (account == null) {
+    account = new BloomAccount(accountID);
+    account.nbActiveAddresses = 0;
+  }
+
+  // handle Address objects
+  for (let i = 0; i < 2; i++) {
+    let addressString: string;
+    addressString =
+      i < 1
+        ? event.params.currentAddress.toHexString()
+        : event.params.newAddress.toHexString();
+    // load Address object
+    let address = BloomAddress.load(addressString);
+    if (address == null) {
+      // create Address object
+      address = new BloomAddress(addressString);
+      address.address = addressString;
+    }
+
+    if (address.account == null) {
+      // create new BloomAddressHistoryItem object
+      let addressHistoryItemID = createAddressHistoryItemID(
+        addressString,
+        event
+      );
+      let addressHistoryItem = new BloomAddressHistoryItem(
+        addressHistoryItemID
+      );
+      addressHistoryItem.linkID = accountID;
+      addressHistoryItem.address = addressString;
+      addressHistoryItem.createdDuringMigration = initializing;
+      addressHistoryItem.creationBlock = blockNumber;
+      addressHistoryItem.save();
+      // update active field in BloomAddress object
+      address.account = accountID;
+      // update nbActiveAddresses field in BloomAccount object
+      account.nbActiveAddresses += 1;
+    }
+    address.save();
+  }
+  account.save();
 }
 
-export function handleUpdatedGravatar(event: UpdatedGravatar): void {
-  let id = event.params.id.toHex()
-  let gravatar = Gravatar.load(id)
-  if (gravatar == null) {
-    gravatar = new Gravatar(id)
+export function handleAddressUnlinked(event: AddressUnlinked): void {
+  // get parameters from event
+  let blockNumber = event.block.number;
+  let addressString = event.params.addressToRemove.toHexString();
+  // load BloomAddress object
+  let address = BloomAddress.load(addressString);
+  if (address == null) return;
+  let addressHistory = address.history;
+  for (let i = 0; i < addressHistory.length; i++) {
+    // load BloomAddressHistoryItem object
+    let addressHistoryItem = BloomAddressHistoryItem.load(addressHistory[i]);
+    if (addressHistoryItem.deletionBlock == null) continue;
+    addressHistoryItem.deletionBlock = blockNumber;
+    addressHistoryItem.save();
+    // update BloomAddress object
+    address.unset("account");
+    address.save();
+    // load BloomAccount object
+    let account = BloomAccount.load(addressHistoryItem.linkID);
+    if (account == null) return;
+    // update BloomAccount object
+    account.nbActiveAddresses -= 1;
+    account.save();
   }
-  gravatar.owner = event.params.owner
-  gravatar.displayName = event.params.displayName
-  gravatar.imageUrl = event.params.imageUrl
-  gravatar.save()
+}
+
+function createAddressHistoryItemID(
+  addressString: string,
+  event: AddressLinked
+): string {
+  return (
+    addressString +
+    "-" +
+    event.transaction.hash.toHex() +
+    "-" +
+    event.logIndex.toString()
+  );
 }
