bbcaea4326f855bdb45d8de179314f19868db3c5
Philippe Métais <phi.metais@gmail.com>
2019-12-22 18:54:12 +0100

    Updates

---
+++ precache-manifest.8ed17d96bf7035cb4eadfc291678665e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3da47861e89b7b340f2d",
+    "url": "/bloom-subgraph/static/js/runtime~main.3da47861.js"
+  },
+  {
+    "revision": "105b8a730124a78b6c44",
+    "url": "/bloom-subgraph/static/js/main.105b8a73.chunk.js"
+  },
+  {
+    "revision": "3727b958565f0d7df390",
+    "url": "/bloom-subgraph/static/js/2.3727b958.chunk.js"
+  },
+  {
+    "revision": "105b8a730124a78b6c44",
+    "url": "/bloom-subgraph/static/css/main.99f3e411.chunk.css"
+  },
+  {
+    "revision": "bba1ec0918a8101bd9d1f78dd1d29771",
+    "url": "/bloom-subgraph/index.html"
+  }
+];
