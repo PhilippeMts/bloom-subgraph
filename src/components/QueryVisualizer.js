3b7305713f77fa3ae055529c634f10c2ef9b41b0
Philippe Métais <phi.metais@gmail.com>
2019-12-23 04:01:45 +0100

    :lipstick: Adding a spinner and enhancing node labels

--- src/components/QueryVisualizer.js
+++ src/components/QueryVisualizer.js
@@ -3,6 +3,7 @@ import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
 import ForceGraph2D from "react-force-graph-2d";
 import bigInt from "big-integer";
+import Loader from "react-loader-spinner";
 
 const QueryVisualizer = () => {
   const MAIN_QUERY = gql`
@@ -14,6 +15,9 @@ const QueryVisualizer = () => {
           id
         }
         attesterOf {
+          id
+        }
+        addresses {
           id
         }
       }
@@ -30,29 +34,42 @@ const QueryVisualizer = () => {
   `;
 
   const { loading, error, data } = useQuery(MAIN_QUERY);
-  if (loading) return "loading";
+  if (loading)
+    return (
+      <div className={"centeringWrapper"}>
+        <Loader type="Grid" color="#0000a0" height={100} width={100} />
+      </div>
+    );
   if (error) return `error: ${error}`;
   const { identities, attestations } = data;
   const nodes = [];
   const links = [];
   for (const identity of identities) {
-    const { id, bltBalance, attesterOf } = identity;
-    let color, val;
+    const { id, bltBalance, subjectOf, attesterOf } = identity;
+    let color, val, name;
     const isAttester = 0 < attesterOf.length;
+    const balanceNumber = bigInt(bltBalance)
+      .divide(bigInt(10).pow(bigInt(18)))
+      .valueOf();
     if (isAttester) {
       val = attesterOf.length;
       color = "#00f000";
+      name = `Role: attester`;
     } else {
-      val = bigInt(bltBalance)
-        .divide(bigInt(10).pow(bigInt(18)))
-        .valueOf();
+      val = balanceNumber;
       color = "#0000f0";
+      name = `Role: User`;
     }
+    name += `, Addresses: [${identity.addresses.map(
+      ({ id }) => `${id.substr(0, 6)}…${id.substr(38, 4)} `
+    )}], \nBLT Balance: ${balanceNumber}, Nb Attestations: ${subjectOf.length +
+      attesterOf.length}`;
     val = Math.min(20, Math.max(1, val));
     nodes.push({
       id,
       val,
-      color
+      color,
+      name
     });
   }
   for (const attestation of attestations) {
@@ -62,7 +79,13 @@ const QueryVisualizer = () => {
     });
   }
   const graphData = { nodes, links };
-  return <ForceGraph2D graphData={graphData} />;
+  return (
+    <ForceGraph2D
+      graphData={graphData}
+      linkDirectionalParticles={2}
+      linkDirectionalParticleWidth={0.64}
+    />
+  );
 };
 
 export default QueryVisualizer;
